b57be9be9e4a90f0569dfc19b54914f0b6119e52
Rio Sanka <threehouse.second@gmail.com>
2020-03-17 12:47:24 +0900

    フォームの作成とdb.jsonの内容表示

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div>hello</div>
-  );
+export default class App extends React.Component  {
+  constructor() {
+    super();
+    this.state = {
+      memo: '',
+      lists: [],
+    }
+    this._onChange = this._onChange.bind(this)
+  }
+
+  componentWillMount(){
+  this.fetchResponse();
 }
 
-export default App;
+fetchResponse(){
+  console.log('done');
+  fetch('http://localhost:3001/memos')
+  .then( res => res.json( ))
+  .then( res => {
+    this.setState({
+      lists : res
+    });
+  })
+}
+
+  _onChange(event) {
+    this.setState({
+      memo: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <form>
+        <div>
+          <label>memo</label>
+          <br />
+          <textarea value={this.state.memo} onChange={this._onChange} />
+        </div>
+        <button type="submit" >保存</button>
+        <ul>
+          {this.state.lists.map((item) => { 
+            return (<li key={item.id}>{item.memo}</li>)
+          })}
+        </ul>
+        {console.log('list:',this.state.lists)}
+      </form>
+    );
+  }
+}
+
+
+
+
+
